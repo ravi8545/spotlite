40b170a714675c360d02d04d117b4b52524554a6
ravi8545 <139250843+ravi8545@users.noreply.github.com>
2026-01-03 23:05:00 +0530

    matrix text

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-addEventListener('mousemove', function(el){
-    console.log(el.clientX, el.clientY);
-    document.body.style.setProperty("--x", el.clientX+'px');
-    document.body.style.setProperty("--y", el.clientY+'px');
+let para = document.querySelector("p");
+let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqestuvwxyz";
+let text = para.innerText;
 
-})
+let iteration=0;
+
+function randomText(){
+    const str = text.split("").map((char, index)=>{
+        if(index < iteration){
+            return char;
+        }
+        return characters.split("")[Math.floor(Math.random() * 53)];
+    })
+    .join("");
+    
+    para.innerText=str;
+ 
+    iteration += 1;
+
+
+
+}
+setInterval(randomText, 1000);
